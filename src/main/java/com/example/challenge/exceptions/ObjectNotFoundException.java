efd75b930ee99b1a53e7162cde7eb94bf1beeaab
gabrielschiferdecke <gabriel.schifer@hotmail.com>
2021-06-26 19:17:31 -0300

    Exclusão de algumas classes inutilizadas

--- src/main/java/com/example/challenge/exceptions/ObjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.challenge.exceptions;
-
-public class ObjectNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public ObjectNotFoundException(String message) {
-        super(message);
-    }
-}
